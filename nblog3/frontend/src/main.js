0590a22864a5678a7e384e6c74eab2bf85ecbd32
naritotakizawa <toritoritorina@gmail.com>
2020-04-21 13:15:28 +0900

    Googleアナリティクスの導入

--- nblog3/frontend/src/main.js
+++ nblog3/frontend/src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -14,3 +15,8 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(VueAnalytics, {
+  id: 'UA-72333380-3',
+  router
+})
